0312c82347a9350600f0b5d8fa41e9bde802b95c
tarun <t.s.clashers@gmail.comn>
2025-07-27 00:18:12 +0530

    made componet and simple hook

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import InputBox from './components/Input'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-red-600'>hello</h1>
+     
+    </>
+  )
+}
+
+export default App
